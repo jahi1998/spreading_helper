c10bd0fa98083c9df7b96d1e0be72742a80881f0
jahi1998 <13691554018@163.com>
2021-08-27 14:26:25 -0500

    Resaved package.json file.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const app =express(); // set up for listening
+const express = require("express");
+const app = express(); // set up for listening
 
-app.get('/',(req,res)) => { 
-    res.send({hi:'there'});
-
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
 });
 
-
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
