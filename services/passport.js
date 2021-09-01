5b53beb1a0bb77912c4d398d4ef7237fc9b6ee94
jahi1998 <13691554018@163.com>
2021-08-31 20:16:24 -0500

    upload all

---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("user");
+
+//set id into model and put piece of info into cookies
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// transfer model into instance, which helpe us retrive the user
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((exsitingUser) => {
+        if (exsitingUser) {
+          done(null, exsitingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
+    }
+  )
+);
