c6aced75aaf367a7bc22ffcae355603be3b68045
0x-cygnet <bloopsocket@gmail.com>
2021-09-17 14:18:11 -0500

    fix imports

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const keys = require("../config/keys");
-require("../models/User");
-require("./passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
@@ -25,8 +25,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("../routes/authRoutes")(app);
-require("../routes/billingRoutes")(app);
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,7 +1,6 @@
 const keys = require('../config/keys');
-const stripe = reqire('stripe')(keys.stripeSecretKey);
-
+const stripe = require('stripe')(keys.stripeSecretKey);
 
 moudle.exports = (app) => {
-  app.post("/api/stripe", (req, res) => {});
+  app.post('/api/stripe', (req, res) => {});
 };
