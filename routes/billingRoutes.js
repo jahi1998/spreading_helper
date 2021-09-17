17d88950387cff3b69abbcc725be6471c2043966
0x-cygnet <bloopsocket@gmail.com>
2021-09-17 14:32:29 -0500

    fix typo in import

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,6 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
-moudle.exports = (app) => {
+module.exports = (app) => {
   app.post('/api/stripe', (req, res) => {});
 };
