31ccdc91c7539400abe5b22a2b309c1af5295939
jahi1998 <13691554018@163.com>
2021-09-27 23:50:23 -0500

    add

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,16 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
+// 
 module.exports = (app) => {
-  app.post('/api/stripe', (req, res) => {});
+  app.post('/api/stripe', async (req, res) => {
+
+    const charge = await stripe.change.create({
+      amount:500,
+      currency: 'usd',
+      description: '$5 for 5 credits'，
+      source:req.body.id
+      
+    })
+  });
 };
